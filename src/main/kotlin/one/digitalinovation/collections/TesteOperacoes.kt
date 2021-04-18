ec14a29a4aeee7e867ddd4f6f7f6354092ea969a
Rubens <rubao.ma@gmail.com>
2021-04-17 23:43:37 -0300

    add count find and any operations

--- src/main/kotlin/one/digitalinovation/collections/TesteOperacoes.kt
+++ src/main/kotlin/one/digitalinovation/collections/TesteOperacoes.kt
@@ -18,4 +18,21 @@ fun main() {
     println("-------------------------")
     salariosMaiorQue2500.forEach { println(it) }
 
+    println("-------------------------")
+    //quantos salarios estão dentro de um range neste caso salarios que estão entre 2000.0 e 5000.0
+    // nesse caso utilizamos o count passando um predicado e cria uma lista nova com a quantidade de elementos verdadeiros
+    println("quant salarios dentro do range ${salarios.count { it in 2000.0..5000.0 }}")
+
+    // agora vamos procurar dentro da lista um salario igual ao valor 2250.0
+    // nesse caso utilizamos o find passando um predicado e cria uma lista nova com os elementos iguais
+    println("-------------------------")
+    println(salarios.find { it == 2250.0 })
+    println(salarios.find { it == 500.0 })
+
+    // Verificar se há um elemento que seja igual ao predicado passado se há retorna true se não há na lista retorna false
+    // utilizamos o any
+    println("-------------------------")
+    println(salarios.any { it == 1000.0 })
+    println(salarios.any { it == 1010.0 })
+
 }
