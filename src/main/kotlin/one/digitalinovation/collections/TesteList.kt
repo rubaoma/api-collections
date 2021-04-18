0f443d9145f3e3f0126c13f9f96bc7a4d3db905f
Rubens <rubao.ma@gmail.com>
2021-04-18 00:09:35 -0300

    add groupby

--- src/main/kotlin/one/digitalinovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinovation/collections/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinovation.collections
 
 fun main() {
-    val joao = Funcionario("João", 2000.0)
-    val pedro = Funcionario("Pedro", 1500.0)
-    val maria = Funcionario("Maria", 4000.0)
+    val joao = Funcionario("João", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
 
     val funcionarios = listOf(joao, pedro, maria)
 
@@ -18,11 +18,17 @@ fun main() {
     println("-------------------------")
     funcionarios.sortedBy { it.salario }.forEach { println(it) }
 
+
+    // fazer o agrupamento mas pelo tipo de contratação
+    println("-------------------------")
+    funcionarios.groupBy { it.tipoContatacao }.forEach { println(it) }
+
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContatacao: String
 ){
     override fun toString(): String =
         """
