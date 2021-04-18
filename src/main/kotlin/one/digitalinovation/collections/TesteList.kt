ef31ecce752e751b7cd82cb15bd015c502999890
Rubens <rubao.ma@gmail.com>
2021-04-17 23:59:59 -0300

    add sortedBy operator

--- src/main/kotlin/one/digitalinovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinovation/collections/TesteList.kt
@@ -1,8 +1,8 @@
 package one.digitalinovation.collections
 
 fun main() {
-    val joao = Funcionario("João", 1000.0)
-    val pedro = Funcionario("Pedro", 2000.0)
+    val joao = Funcionario("João", 2000.0)
+    val pedro = Funcionario("Pedro", 1500.0)
     val maria = Funcionario("Maria", 4000.0)
 
     val funcionarios = listOf(joao, pedro, maria)
@@ -13,6 +13,10 @@ fun main() {
 
     println("-------------------------")
     println(funcionarios.find { it.nome == "Maria" })
+
+    // fazer a ordenação mas pelo salário
+    println("-------------------------")
+    funcionarios.sortedBy { it.salario }.forEach { println(it) }
 
 }
 
