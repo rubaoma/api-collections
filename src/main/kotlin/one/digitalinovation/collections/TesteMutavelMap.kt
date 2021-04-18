ff2fae712b1b0d6b0d309ad2e2c34bca1f2a125a
Rubens <rubao.ma@gmail.com>
2021-04-18 15:26:52 -0300

    add Repositorio.kt class and TesteMutavelMap.kt class

---
+++ src/main/kotlin/one/digitalinovation/collections/TesteMutavelMap.kt
@@ -0,0 +1,17 @@
+package one.digitalinovation.collections
+
+fun main() {
+    val joao = Funcionario("João", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
+
+    val repositorio = Repositorio<Funcionario>()
+
+    repositorio.create(joao.nome, joao)
+    repositorio.create(pedro.nome, pedro)
+    repositorio.create(maria.nome, maria)
+
+    println(repositorio.findById(maria.nome))
+
+
+}
