c5ab6ecf126e6c71f4f22f5a51b2e555cd72be7f
Rubens <rubao.ma@gmail.com>
2021-04-17 23:55:05 -0300

    add test list class

---
+++ src/main/kotlin/one/digitalinovation/collections/TesteList.kt
@@ -0,0 +1,28 @@
+package one.digitalinovation.collections
+
+fun main() {
+    val joao = Funcionario("João", 1000.0)
+    val pedro = Funcionario("Pedro", 2000.0)
+    val maria = Funcionario("Maria", 4000.0)
+
+    val funcionarios = listOf(joao, pedro, maria)
+
+    funcionarios.forEach {
+        println(it)
+    }
+
+    println("-------------------------")
+    println(funcionarios.find { it.nome == "Maria" })
+
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double
+){
+    override fun toString(): String =
+        """
+            Nome:    $nome
+            Salário: $salario
+        """.trimIndent()
+}
